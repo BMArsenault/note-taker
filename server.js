5eb3274f223c6b9ec592955c3f248803b8cb31a0
Brian <BMArsenault@gmail.com>
2021-12-17 11:20:22 -0500

    Added notesFile.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+const notesFile = require("./db/notesFile");
+
 // const apiRoutes = require('./routes/apiRoutes');
 // const htmlRoutes = require('./routes/htmlRoutes');
 
@@ -17,7 +19,31 @@ app.use(express.static('public'));
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 
+app.get('/api/notes', (req,res) => {
+    res.json(notes);
+});
 
+// Delete a candidate
+// app.delete('/api/notes/:id', (req, res) => {
+//     const sql = `DELETE FROM notes WHERE id = ?`;
+//     const params = [req.params.id];
+  
+//     db.query(sql, params, (err, result) => {
+//       if (err) {
+//         res.statusMessage(400).json({ error: res.message });
+//       } else if (!result.affectedRows) {
+//         res.json({
+//           message: 'Notes not found'
+//         });
+//       } else {
+//         res.json({
+//           message: 'deleted',
+//           changes: result.affectedRows,
+//           id: req.params.id
+//         });
+//       }
+//     });
+// });
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
