21ec5929bf8a36adc8ab8192a45eebc4c7987efe
Brian <BMArsenault@gmail.com>
2022-01-25 20:11:53 -0700

    Added delete function

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const fs = require('fs');
-const PORT = process.env.PORT || 3001;
-const app = express();
 const path = require('path');
 const notes = require('./db/db.json');
 const {v4: uuidv4} = require('uuid');
 //const router = require('express').Router()
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
-// const notesFile = require("./db/notesFile");
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'./public/index.html'));
@@ -39,14 +38,18 @@ app.post('/api/notes', (req, res) => {
   res.sendStatus(200);
 });
 
-// app.delete('/api/notes/:id', (req, res) => {
-//   if(req.body.id) {
-//     req.body.id = uuidv4();
-//   }
-//   notes.push(req.body)
-//   fs.writeFileSync('./db/db.json', JSON.stringify(notes))
-//   res.sendStatus(200);
-// });
+app.delete('/api/notes/:id', (req, res) => {
+  var newNotes = notes.map((item) => { return item.id; }).indexOf(req.params.id); //find the index of :id
+  if(newNotes === -1) {
+    res.statusCode = 404;
+    return res.send('Error 404: No quote found');
+  }
+  var result = notes.splice(newNotes,1);
+  fs.writeFile('./db/db.json', JSON.stringify(result), function(err){
+   if(err) throw err;
+   res.json(true);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
